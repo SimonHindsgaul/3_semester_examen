bb1ffe651d82d0a7a9bf0e5743ce27ee80cc5caa
SimonHindsgaul <79086677+SimonHindsgaul@users.noreply.github.com>
2021-09-15 11:10:04 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*SIMAX V. 0.1.0. - Fagmodulkursus 1; Essential Computing Skriftlig Aflevering til eksamen
+Lavet af:
+Max Andreas de Visser, Studie nr.: 69359
+Simon Hindsgaul, Studie nr.: 69227
+ */
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Cykelhandler cykelhandler = new Cykelhandler(); //kalder konstruktoren og opretter en cykelhandler
+        cykelhandler.samtale(); //initierer samtalen
+    }
+}
